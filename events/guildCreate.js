6c943fbfe46183c5e52df7591874edb74bc048f4
unknown <suvajit.c@outlook.in>
2018-11-16 10:24:00 +0530

    Update

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,3 +1,4 @@
 module.exports = (client, guild) => {
     console.log(`New Server added \nName: ${guild.name} \nID: ${guild.id}`);
+    client.user.setActivity(`!help for options`, {type: "WATCHING"});
 }
