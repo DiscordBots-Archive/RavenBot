5639674e5259dba2012bbb545fc38cf45ed23667
unknown <suvajit.c@outlook.in>
2018-11-16 16:53:45 +0530

    Update

--- events/message.js
+++ events/message.js
@@ -3,18 +3,32 @@ module.exports = (client, message) => {
     if (message.author.bot) return;
   
     // Ignore messages not starting with the prefix (in config.json)
-    if (message.content.indexOf(client.config.prefix) !== 0) return;
+    //if (message.content.indexOf(client.config.prefix) !== 0) return;
   
     // Our standard argument/command name definition.
-    const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
+    //const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
+    const prefix = (config.prefix);
+    const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|\\${prefix})\\s*`);
+    if (!prefixRegex.test(message.content)) return;
+    const [, matchedPrefix] = message.content.match(prefixRegex);
+    const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
 
     // Grab the command data from the client.commands Enmap
     const cmd = client.commands.get(command);
   
     // If that command doesn't exist, silently exit and do nothing
-    if (!cmd) return;// message.channel.send(`${message.author.username}: `+ "Command Not Found! Please do **!help**")
+    if (!cmd) return;
   
     // Run the command
     cmd.run(client, message, args);
   };
+
+  /*
+  const prefix = (config.prefix);
+  const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|\\${prefix})\\s*`);
+  if (!prefixRegex.test(message.content)) return;
+  const [, matchedPrefix] = message.content.match(prefixRegex);
+  const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
+  const command = args.shift();
+  */
