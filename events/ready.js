2636903000ffeaae17fcb4bd3d63669214fe25ba
SUVAJIT <suvajit.c@outlook.in>
2018-11-17 07:17:26 +0530

    Update

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 module.exports = (client) => {
     console.log(`logging In \nClient: ${client.user.tag} \nUsers: ${client.users.size} \nChannels: ${client.channels.size} \nServers: ${client.guilds.size}`);
-    client.user.setActivity(`!help for options`, {type: "PLAYING"});
+    client.user.setActivity(`@ECHO5 help`, {type: "PLAYING"});
 }
