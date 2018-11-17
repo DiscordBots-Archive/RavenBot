30c1a94bf6a86acc7622b3013733ce32c0aee3d4
SUVAJIT <suvajit.c@outlook.in>
2018-11-17 20:14:15 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 module.exports = (client) => {
     console.log(`logging In \nClient: ${client.user.tag} \nUsers: ${client.users.size} \nChannels: ${client.channels.size} \nServers: ${client.guilds.size}`);
-    client.user.setActivity(`!help`, {type: "PLAYING"});
+    client.user.setActivity(`${process.env.DISCORD_PREFIX}help`, {type: "PLAYING"});
 }
