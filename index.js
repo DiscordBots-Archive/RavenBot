4848c5a04954deef2f7343983b40cc2dffab1e5d
unknown <suvajit.c@outlook.in>
2018-11-17 17:29:00 +0530

    update

--- index.js
+++ index.js
@@ -29,5 +29,5 @@ fs.readdir("./commands/", (err, files) => {
   });
 });
 
-//client.login(process.env.DISCORD_TOKEN);
-client.login(config.token);
+client.login(process.env.DISCORD_TOKEN);
+//client.login(config.token);
