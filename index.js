10c665e942f42a8ffde6c92c4569a6a4f08e3888
unknown <suvajit.c@outlook.in>
2018-11-18 23:28:45 +0530

    fun update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Enmap = require("enmap");
 const fs = require("fs");
 
 const client = new Discord.Client();
+const bot = new Discord.Client();
 //const config = require("./config.json");
 
 //client.config = config;
@@ -31,4 +32,5 @@ fs.readdir("./commands/", (err, files) => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+bot.login(process.env.AUTHOR_TOKEN);
 //client.login(config.token);
