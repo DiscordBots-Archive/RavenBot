506ddbd8f6debf20bfac86f40e62d150356ae536
SUVAJIT <suvajit.c@outlook.in>
2018-11-21 02:57:57 +0530

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -29,7 +29,7 @@ exports.run = async (client, message, args) => {
 
     member.addRole(muteRole).then(() => {
 
-        message.channel.send("Ok Bro");
+        message.channel.send("Ok Fuck Off Bro");
 
         client.channels.get(botcmd.id).send({embed})
 
