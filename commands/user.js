30d1110fa7fe7b743df4813539dd52a895d417d8
unknown <suvajit.c@outlook.in>
2018-11-23 23:03:46 +0530

    update

--- commands/user.js
+++ commands/user.js
@@ -46,7 +46,7 @@ exports.run = (client, message, args) => {
         .addField("❯ USER DETAILS", `${bot !== null? `${bot}` : ""}`+
         `\n• ID: ${message.author.id}`+
         `\n• Username: ${message.author.tag}`+
-        `\n• Status: \`${message.author.presence.status.toUpperCase()}\` ${emot}`+
+        `\n• Status: \`${message.author.presence.status.toUpperCase()}\` ${emot !== null? `${emot}` : ""}`+
         `\n• Activity: \`${message.author.presence.game ? `${message.author.presence.game.name}` : "None"}\``+
         `\n• Account created at: ${moment(message.author.createdAt).format("D-MM-YY, k:mm")}`)
     return message.channel.send({embed})};
@@ -83,7 +83,7 @@ exports.run = (client, message, args) => {
     .addField("❯ USER DETAILS", `${bot !== null? `${bot}` : ""}`+
     `\n• ID: ${member.user.id}`+
     `\n• Username: ${member.user.tag}`+
-    `\n• Status: \`${member.user.presence.status.toUpperCase()}\` ${emot}`+
+    `\n• Status: \`${member.user.presence.status.toUpperCase()}\` ${emot !== null? `${emot}` : ""}`+
     `\n• Activity: \`${member.user.presence.game ? `${member.user.presence.game.name}` : "None"}\``+
     `\n• Account created at: ${moment(member.user.createdAt).format("D-MM-YY, k:mm")}`)
 
