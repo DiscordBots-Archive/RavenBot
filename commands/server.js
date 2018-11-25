0583aa818b735486827de1920c150ceb7ed4bbc9
unknown <suvajit.c@outlook.in>
2018-11-25 23:24:49 +0530

    update

--- commands/server.js
+++ commands/server.js
@@ -16,11 +16,15 @@ exports.run = (client, message, args) => {
     .setFooter(`Requested by ${message.author.username}`, 'https://discordemoji.com/assets/emoji/DiscordHype.gif')
     .setTimestamp()
 
-    .addField("❯ CHANNELS & ROLES", `• Category: ${message.guild.channels.size - message.guild.channels.filter(ch => ch.type === 'text').size - message.guild.channels.filter(ch => ch.type === 'voice').size}`+
-    `\n• Total: ${message.guild.channels.filter(ch => ch.type === 'text').size + message.guild.channels.filter(ch => ch.type === 'voice').size}`+
-    `\n• Text: ${message.guild.channels.filter(ch => ch.type === 'text').size}`+ 
+    .addField("❯ CHANNELS", `• Total: ${message.guild.channels.filter(ch => ch.type === 'text').size + message.guild.channels.filter(ch => ch.type === 'voice').size}`+
+    `\n• Category: ${message.guild.channels.size - message.guild.channels.filter(ch => ch.type === 'text').size - message.guild.channels.filter(ch => ch.type === 'voice').size}`+
+    `\n${message.guild.channels.filter(ch => ch.type === 'category').map(channels => `\`${channels.name}\``).join(' ')}`+
+    `\n• Text: ${message.guild.channels.filter(ch => ch.type === 'text').size}`+
+    `\n${message.guild.channels.filter(ch => ch.type === 'text').map(channels => `\`${channels.name}\``).join(' ')}`+
     `\n• Audio: ${message.guild.channels.filter(ch => ch.type === 'voice').size}`+
-    `\n• Roles: ${message.guild.roles.size}`)
+    `\n${message.guild.channels.filter(ch => ch.type === 'voice').map(channels => `\`${channels.name}\``).join(' ')}`)
+
+    .addField('❯ ROLES', `• Roles: ${message.guild.roles.size} \n${message.guild.roles.map(roles => `\`${roles.name}\``).join(' ')}`)
 
     .addField("❯ MEMBERS", `• Total: ${message.guild.memberCount}`+
     `\n• Bots : ${message.guild.members.filter(m => m.user.bot).size}`+
