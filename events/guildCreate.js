ad01199f7bb07236fb0691f7134c78b3b4ff4520
unknown <suvajit.c@outlook.in>
2018-11-25 15:31:48 +0530

    update

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,7 +1,7 @@
 module.exports = (client, guild) => {
     console.log(`New Server added \nName: ${guild.name} \nID: ${guild.id}`);
 
-    /*const channel = client.channels.find(ch => ch.id === '516098181549916172');
+    const channel = client.channels.find(ch => ch.id === '516098181549916172');
     channel.send(`SERVER ADDED : ${guild.name} \nSERVER ID : ${guild.id} \nUSERS : ${client.users.size} \nCHANNELS : ${client.channels.size} \nNOW TOTAL SERVERS : ${client.guilds.size}`);
-*/
+
 }
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,7 +1,7 @@
 module.exports = (client, guild) => {
     console.log(`Server Removed \nName ${guild.name} \nID: ${guild.id}`);
 
-  /*  const channel = client.channels.find(ch => ch.id === '516098181549916172');
+    const channel = client.channels.find(ch => ch.id === '516098181549916172');
     channel.send(`SERVER REMOVED : ${guild.name} \nSERVER ID : ${guild.id} \nUSERS : ${client.users.size} \nCHANNELS : ${client.channels.size} \nNOW TOTAL SERVERS : ${client.guilds.size}`);
-*/
+
 }
