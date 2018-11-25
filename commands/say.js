7e80d982776a4890a82069c501825b3f36c86d41
unknown <suvajit.c@outlook.in>
2018-11-25 22:32:55 +0530

    update

--- commands/say.js
+++ commands/say.js
@@ -1,10 +1,10 @@
 exports.run = async (client, message, args) => {
 
-    if (message.channel.name !== 'bot-commands' & message.channel.type !== 'dm' & message.channel.name !== 'change-log' & message.channel.name !== 'read-me' & message.channel.name !== 'audio-commands') {
+    /*if (message.channel.name !== 'bot-commands' & message.channel.type !== 'dm' & message.channel.name !== 'change-log' & message.channel.name !== 'read-me' & message.channel.name !== 'audio-commands') {
         let channel = message.guild.channels.find(ch => ch.name === "bot-commands");
         if(!channel) return message.channel.send('Could not found **#bot-commands** channel. Please create it and try again.')
         return message.channel.send(`Please use commands in appropriate chatrooms to reduce spam ${channel}`);
-    }
+    }*/
     const sayMessage = args.join(" ");
     if (!args.join(" ")) {
         return message.channel.send(`${message.author.username}: ` + "Please provide a message!");
