49866f8b9ae8084610ad877d182db4ccd3e51ba2
SUVAJIT <suvajit.c@outlook.in>
2018-11-28 23:52:27 +0530

    update

--- commands/clear.js
+++ commands/clear.js
@@ -1,9 +1,14 @@
 exports.run = async (client, message, args) => {
+    if (message.guild.id == '500004711005683717') {
+    if(!message.member.roles.some(r=>[process.env.DEV_ROLE].includes(r.name)) )
+    return message.channel.send(`${message.author.username} : `+ `This command has been disabled.`);
+    
     const deleteCount = parseInt(args[0], 10);
     if(!deleteCount || deleteCount < 2 || deleteCount > 100)
     return message.channel.send(`${message.author.username}: ` + "Please provide a number between 2 and 100 for the number of messages to delete");
     const fetched = await message.channel.fetchMessages({limit: deleteCount});
     message.channel.bulkDelete(fetched)
     .catch(error => message.channel.send(`${message.author.username}: ` + `Couldn't delete messages because of: ${error}`));
+        }
 
 }
