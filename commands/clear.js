39907f6793b366853b7aee6d2a14c8bd555c49be
SUVAJIT <suvajit.c@outlook.in>
2018-11-30 02:35:56 +0530

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,6 +1,6 @@
 exports.run = async (client, message, args) => {
     
-    const user = message.mentions.users.first();
+    /*const user = message.mentions.users.first();
 // Parse Amount
 const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
 if (!amount) return message.reply('Must specify an amount to delete!');
@@ -17,16 +17,20 @@ message.channel.fetchMessages({
            const messages =  message.channel.fetchMessages({limit: amount});
     }
  message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
-});
+});*/
     
     
-    /*if (message.guild.id == '500004711005683717') {
+    if (message.guild.id == '500004711005683717') {
     if(!message.member.roles.some(r=>[process.env.DEV_ROLE].includes(r.name)) )
     return message.channel.send(`${message.author.username} : `+ `This command has been disabled.`);
-    
+    const user = message.mentions.users.first();
     const deleteCount = parseInt(args[0], 10);
     if(!deleteCount || deleteCount < 2 || deleteCount > 100)
     return message.channel.send(`${message.author.username}: ` + "Please provide a number between 2 and 100 for the number of messages to delete");
+    if (user) {
+ const filterBy = user ? user.id : Client.user.id;
+ fetched = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
+ }
     const fetched = await message.channel.fetchMessages({limit: deleteCount});
     message.channel.bulkDelete(fetched)
     .catch(error => message.channel.send(`${message.author.username}: ` + `Couldn't delete messages because of: ${error}`));
@@ -41,6 +45,6 @@ message.channel.fetchMessages({
     const fetched = await message.channel.fetchMessages({limit: deleteCount});
     message.channel.bulkDelete(fetched)
     .catch(error => message.channel.send(`${message.author.username}: ` + `Couldn't delete messages because of: ${error}`));
-        }*/
+        }
 
 }
