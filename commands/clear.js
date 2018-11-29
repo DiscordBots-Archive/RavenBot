4bc0c55c60f68651536fdaee5354f64824487d62
SUVAJIT <suvajit.c@outlook.in>
2018-11-30 02:06:50 +0530

    update

--- commands/clear.js
+++ commands/clear.js
@@ -1,5 +1,23 @@
 exports.run = async (client, message, args) => {
-    if (message.guild.id == '500004711005683717') {
+    
+    const user = message.mentions.users.first();
+// Parse Amount
+const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
+if (!amount) return message.reply('Must specify an amount to delete!');
+if (!amount && !user) return message.reply('Must specify a user and amount, or just an amount, of messages to purge!');
+// Fetch 100 messages (will be filtered and lowered up to max amount requested)
+message.channel.fetchMessages({
+ limit: 100,
+}).then((messages) => {
+ if (user) {
+ const filterBy = user ? user.id : Client.user.id;
+ messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
+ }
+ message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
+});
+    
+    
+    /*if (message.guild.id == '500004711005683717') {
     if(!message.member.roles.some(r=>[process.env.DEV_ROLE].includes(r.name)) )
     return message.channel.send(`${message.author.username} : `+ `This command has been disabled.`);
     
@@ -20,6 +38,6 @@ exports.run = async (client, message, args) => {
     const fetched = await message.channel.fetchMessages({limit: deleteCount});
     message.channel.bulkDelete(fetched)
     .catch(error => message.channel.send(`${message.author.username}: ` + `Couldn't delete messages because of: ${error}`));
-        }
+        }*/
 
 }
