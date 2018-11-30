62540848eacbbce64f4670c71c88fc960bb946ba
SUVAJIT <suvajit.c@outlook.in>
2018-12-01 00:05:36 +0530

    Update usr.js

--- commands/usr.js
+++ commands/usr.js
@@ -1,4 +1,4 @@
 exports.run = (client, message, args) => {
   //return;
-message.channel.send(`{client.guilds.map(g=>g.name).join('\n')}`)
+message.channel.send(`${client.guilds.map(g=>g.name).join('\n')}`)
 }
