08bff143b46055be78a24b6cae3df15004560250
SUVAJIT <suvajit.c@outlook.in>
2018-12-01 00:19:49 +0530

    Update message.js

--- events/message.js
+++ events/message.js
@@ -15,4 +15,9 @@ module.exports = (client, message) => {
   if (!cmd) return message.channel.send(`Command not found, please do \`${process.env.DISCORD_PREFIX}help\` for options`)
 
   cmd.run(client, message, args);
+  const swearWords = ["darn", "shucks", "frak", "shite"];
+if( swearWords.some(word => message.content.includes(word)) ) {
+  message.reply("Oh no you said a bad word!!!");
+  // Or just do message.delete();
+}
 };
