b52acdb100e066017c6647eabace7be3efbfb9a9
unknown <suvajit.c@outlook.in>
2018-11-30 23:16:38 +0530

    update

--- commands/player.js
+++ commands/player.js
@@ -17,7 +17,7 @@ exports.run = async (client, message, args) => {
     m.edit(`Player Details: \nhttp://kuilin.net/cc_n/member.php?tag=${playertag}`);
     const embed = new Discord.RichEmbed()
     .setColor('#1E90ff')
-    .setTitle("TAP HERE TO DIRECTLY OPEN IN GAME <:clash:516318617357254672>")
+    .setTitle("TAP HERE TO DIRECTLY OPEN IN GAME <:clash:512947779916529675>")
     .setURL(`https://link.clashofclans.com/?action=OpenPlayerProfile&tag=${playertag}`)
     message.channel.send({embed});
 }
