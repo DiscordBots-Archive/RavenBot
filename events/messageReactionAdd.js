687b15f86edb5a45019c5cedf45413d4541e4a80
unknown <suvajit.c@outlook.in>
2018-12-02 22:55:33 +0530

    update

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,30 +1,28 @@
 const Discord = require('discord.js');
 
 module.exports = (client, reaction, user) => {
-  console.log(`${user.username} reacted with "${reaction.emoji.name}".`);
-  let role
-    
+  //console.log(`${user.username} reacted with "${reaction.emoji.name}".`);
+
   const msg = reaction.message;
-  let member = msg.guild.members.get(user.id);
-    
+
   if (msg.id == process.env.REACT_MESSAGE) {
+    let role
+    let member = msg.guild.members.get(user.id);
 
     console.log('Valid Message Reaction')
     if (reaction.emoji.id == '509629414120882176') {
       role = msg.guild.roles.get('500683488538656768')
-      console.log('Valid Emoji Reaction')
-    } 
+      console.log('Role Given')
+    } else if (reaction.emoji.id !== '509629414120882176') return;
     member.addRole(role);
 
     let botcmd = msg.guild.channels.find(ch => ch.name === "bot-spam");
 
     const embed = new Discord.RichEmbed()
-  
     .setColor(65280)
     .setTimestamp()
     .setDescription(`\`❯ VERIFIED BY REACTING \n• ${user.username} has been verified by ${client.user.username}\``)
-
-    client.channels.get(botcmd.id).send({embed});
-
-  }
+    client.channels.get(botcmd.id).send({embed})
+    
+  } else if (msg.id == process.env.REACT_MESSAGE) return;
 }
--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -1,30 +1,28 @@
 const Discord = require('discord.js');
 
 module.exports = (client, reaction, user) => {
-  console.log(`${user.username} removed their "${reaction.emoji.name}" reaction.`);
-  let role
-    
-  const msg = reaction.message;
-  let member = msg.guild.members.get(user.id);
-      
-  if (msg.id == process.env.REACT_MESSAGE) {
+  //console.log(`${user.username} removed their "${reaction.emoji.name}" reaction.`);
 
-    console.log('Valid Message Reaction')
-    if (reaction.emoji.id == '509629414120882176') {
-      role = msg.guild.roles.get('500683488538656768')
-      console.log('Valid Emoji Reaction')
-    } 
+  const msg = reaction.message;
+
+  if (msg.id == process.env.REACT_MESSAGE) {
+  let role
+  let member = msg.guild.members.get(user.id);
+  
+  console.log('Valid Message Reaction')
+  if (reaction.emoji.id == '509629414120882176') {
+    role = msg.guild.roles.get('500683488538656768')
+    console.log('Role Given')
+    } else if (reaction.emoji.id !== '509629414120882176') return;
     member.removeRole(role);
 
     let botcmd = msg.guild.channels.find(ch => ch.name === "bot-spam");
 
     const embed = new Discord.RichEmbed()
-  
     .setColor("#f32d11")
     .setTimestamp()
     .setDescription(`\`❯ UN-VERIFIED BY REACTING \n• ${user.username} has been un-verified by ${client.user.username}\``)
-
     client.channels.get(botcmd.id).send({embed});
-
-  }
+      
+  } else if (msg.id == process.env.REACT_MESSAGE) return;
 }
