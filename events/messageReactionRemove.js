4f8e128c75f7ec1c20650242190a8bdf88f8e77e
unknown <suvajit.c@outlook.in>
2018-12-02 11:59:06 +0530

    update

---
+++ events/messageReactionRemove.js
@@ -0,0 +1,17 @@
+module.exports = (client, reaction, user) => {
+  console.log(`${user.username} removed their "${reaction.emoji.name}" reaction.`);
+  let role
+    
+  const msg = reaction.message;
+  let member = msg.guild.members.get(user.id);
+      
+  if (msg.id == '518671831482236929') {
+
+    console.log('Valid Message Reaction')
+    if (reaction.emoji.id == '509629414120882176') {
+      role = msg.guild.roles.get('500683488538656768')
+      console.log('Valid Emoji Reaction')
+    } 
+    member.removeRole(role);
+  }
+}
