2e1df6526c1198eb57d09ced924819da3d7d54ae
unknown <suvajit.c@outlook.in>
2018-12-05 00:05:27 +0530

    update

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const log = winston.createLogger({
 const tg = new Telegram(process.env.TELEGRAM, { polling: true });
 const mentionHook = new Discord.WebhookClient(process.env.WEBHOOK_ID, process.env.WEBHOOK_TOKEN);
 
-dc.on('message', (message) => {
+client.on('message', (message) => {
 
   // ignore self
   if(message.author.id == client.user.id) return;
