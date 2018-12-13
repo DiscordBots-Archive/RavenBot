dedcbc900bb49fa7d750cac1d3447df4166327d4
unknown <suvajit.c@outlook.in>
2018-12-13 22:07:57 +0530

    update

--- commands/kick.js
+++ commands/kick.js
@@ -2,23 +2,23 @@ const Discord = require('discord.js');
 exports.run = async (client, message, args) => {
   if (message.channel.type == 'dm') {
     message.channel.send("This is Not a right place to use this Command!");
-  };
+  }
 
   if (!message.member.roles.some(r=>['Dev', 'Admin'].includes(r.name)) ) {
     message.channel.send(`Only Admins can use this Command`);
-  };
+  }
 
   let member = message.mentions.members.first();
 
   if (!member) {
     message.channel.send(`Please mention a valid member of this Server!`);
-  };
+  }
 
   if (member == message.guild.members.get(message.author.id)) {
     message.channel.send("Don't kick yourself Idiot!");
   } else if (member == message.guild.members.get(client.user.id)) {
     message.channel.send("You can't kick me!");
-  };
+  }
 
   if (member.roles.has('513284645274517504')) {
     message.channel.send("You can't kick a Staff!");
@@ -26,16 +26,16 @@ exports.run = async (client, message, args) => {
     message.channel.send("You can't kick an Admin");
   } else if (member.roles.has('500683949018710036')) {
     message.channel.send("You can't kick an Admin!");
-  };
+  }
 
   if (!member.kickable) {
     message.channel.send("I could not kick this user!");
-  };
+  }
 
   let reason = args.join('');
   if (!reason) {
     reason = "";
-  };
+  }
 
   await member.kick(reason)
   .catch(error => message.channel.send(`I could not kick this user! \n ${error}`));
@@ -50,5 +50,5 @@ exports.run = async (client, message, args) => {
   .setFooter(`Kicked`)
 
   client.channels.get(mod_log_channel.id).send({embed});
-  message.channel.send("Done. User has been Kicked <a:hype:515571561345056783>")
+  message.channel.send("Done. User has been Kicked <a:hype:515571561345056783>");
 }
