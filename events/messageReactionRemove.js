7612e57957270c72f45a96263e150502fdd32ba6
unknown <suvajit.c@outlook.in>
2018-12-14 21:42:19 +0530

    update

--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -5,16 +5,16 @@ module.exports = (client, reaction, user) => {
 
   const msg = reaction.message;
 
-  if (msg.id == client.config.reaction.message_id) {
+  if (msg.id == '518712940615172096') {
 
     let role
     let member = msg.guild.members.get(user.id);
   
     console.log('Valid Message Reaction')
-    if (reaction.emoji.id == client.config.reaction.emoji_id) {
-      role = msg.guild.roles.get(client.config.reaction.react_role)
+    if (reaction.emoji.id == '509629414120882176') {
+      role = msg.guild.roles.get('500683488538656768')
       console.log('Role Removed')
-    } else if (reaction.emoji.id !== client.config.reaction.emoji_id) return;
+    } else if (reaction.emoji.id !== '509629414120882176') return;
     
     member.removeRole(role);
 
@@ -27,5 +27,5 @@ module.exports = (client, reaction, user) => {
     .setTitle(`${user.username} | ${user.id}`)
     client.channels.get(botcmd.id).send({embed});
       
-  } else if (msg.id !== client.config.reaction.message_id) return;
+  } else if (msg.id !== '518712940615172096') return;
 }
