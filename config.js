9b2c99cc80ed1e8a2319e68e4c6af6bd73b2d7b8
unknown <suvajit.c@outlook.in>
2018-12-14 21:49:31 +0530

    update.......

--- config.js
+++ config.js
@@ -11,30 +11,12 @@ const config = {
         "chat_id" : process.env.TELEGRAM_CHAT_ID,
         "log" : process.env.TELEGRAM_LOG
 
-    },"server" : {
-
-        "guild_id" : process.env,
-        "channel_id" : process.env,
-
-    },"logchannel" : {
-
-        "memberlog_ch_id" : process.env,
-        "modlog_ch_id" : process.env.MODLOG_CHANNEL,
-
-    },"reaction" : {
-
-        "message_id" : process.env.REACT_MSG_ID,
-        "react_role" : process.env.REACT_ROLE,
-        "emoji_id" : process.env.EMOJI_ID
-
     },"webhooks" : {
 
         "guild_id" : process.env.WEBHOOK_GUILD_ID,
         "channel_id" : process.env.WEBHOOK_CHANNEL_ID,
-
         "id" : process.env.WEBHOOK_ID,
         "token" : process.env.WEBHOOK_TOKEN,
-
     },
 
 }
