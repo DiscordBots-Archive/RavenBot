ed263fba037c49df3b409df5fcf748ee57daef1a
unknown <suvajit.c@outlook.in>
2018-12-14 13:36:06 +0530

    update

--- commands/help.js
+++ commands/help.js
@@ -23,24 +23,24 @@ exports.run = (client, message, args) => {
         .setThumbnail('https://discordemoji.com/assets/emoji/DiscordHype.gif')
         .setTimestamp()
     
-        .addField("❯ HELP", `\`• ${process.env.DISCORD_PREFIX}help\`` +' '+ `\`It explains itself!\``)
+        .addField("❯ HELP", `\`• ${process.env.DISCORD_PREFIX}help\`` +' '+ `\`- It explains itself!\``)
     
-        .addField("❯ INFO", `\`• ${process.env.DISCORD_PREFIX}stats\`` +' '+ `\`It shows the bot info\`` + '\n' +
-        `\`• ${process.env.DISCORD_PREFIX}server\`` +' '+ `\`It shows server info\`` + '\n' + 
-        `\`• ${process.env.DISCORD_PREFIX}user @user\`` +' '+ `\`It shows the user info\`` + '\n' +
-        `\`• ${process.env.DISCORD_PREFIX}ping\`` +' '+ `\`It shows the ping status\``)
+        .addField("❯ INFO", `\`• ${process.env.DISCORD_PREFIX}stats\`` +' '+ `\`- It shows the bot info\`` + '\n' +
+        `\`• ${process.env.DISCORD_PREFIX}server\`` +' '+ `\`- It shows server info\`` + '\n' + 
+        `\`• ${process.env.DISCORD_PREFIX}user @user\`` +' '+ `\`- It shows the user info\`` + '\n' +
+        `\`• ${process.env.DISCORD_PREFIX}ping\`` +' '+ `\`- It shows the ping status\``)
 
-        .addField("❯ MOD", `\`• ${process.env.DISCORD_PREFIX}kick @user\`` +' '+ `\`To kick a user\`` + '\n' +
-        `\`• ${process.env.DISCORD_PREFIX}ban @user\`` +' '+ `\`To ban a user\`` + '\n' +
-        `\`• ${process.env.DISCORD_PREFIX}mute @user\`` +' '+ `\`To mute a user\`` + '\n' +
-        `\`• ${process.env.DISCORD_PREFIX}unmute @user\`` +' '+ `\`To unmute a user\``)
+        .addField("❯ MOD", `\`• ${process.env.DISCORD_PREFIX}kick @user\`` +' '+ `\`- To kick a user\`` + '\n' +
+        `\`• ${process.env.DISCORD_PREFIX}ban @user\`` +' '+ `\`- To ban a user\`` + '\n' +
+        `\`• ${process.env.DISCORD_PREFIX}mute @user\`` +' '+ `\`- To mute a user\`` + '\n' +
+        `\`• ${process.env.DISCORD_PREFIX}unmute @user\`` +' '+ `\`- To unmute a user\``)
 
         .addField('ADMIN', `\`• ${process.env.DISCORD_PREFIX}perms @user [add/remove] [verified/staff/coadmin/admin]\`` + '\n' +
         `\`• To add permissions to the user\``)
     
-        .addField("❯ UTILS", `\`• ${process.env.DISCORD_PREFIX}clear [optional @user]\`` +' '+ `\`clear messages\`` + '\n' +
-        `\`• ${process.env.DISCORD_PREFIX}player tag\`` +' '+ `\`Player Lookup\`` + '\n' +
-        `\`• ${process.env.DISCORD_PREFIX}clan tag\`` +' '+ `\`Clan Lookup\``)
+        .addField("❯ UTILS", `\`• ${process.env.DISCORD_PREFIX}clear @user\`` +' '+ `\`- Clear messages\`` + '\n' +
+        `\`• ${process.env.DISCORD_PREFIX}player #tag\`` +' '+ `\`- Player Lookup\`` + '\n' +
+        `\`• ${process.env.DISCORD_PREFIX}clan #tag\`` +' '+ `\`- Clan Lookup\``)
 
         .addField("❯ MEE6", `\`\n• !rank\`` +' '+ `\`It shows your rank\`` + '\n' +
         `\`• !levels\`` +' '+ `\`It shows the position\``)
