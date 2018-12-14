1c3e52ed6b5e917ac04359867c586576833ec189
unknown <suvajit.c@outlook.in>
2018-12-14 13:27:50 +0530

    update

--- commands/help.js
+++ commands/help.js
@@ -23,32 +23,6 @@ exports.run = (client, message, args) => {
         .setThumbnail('https://discordemoji.com/assets/emoji/DiscordHype.gif')
         .setTimestamp()
     
-        .addField("`❯ HELP - IT EXPLAINS ITSELF`", `\`• ${process.env.DISCORD_PREFIX}help\``)
-    
-        .addField("`❯ INFO - CHECK STATISTICAL INFO OF THE BOT, SERVER INFO, USER INFO & PING STATUS`", `\`• ${process.env.DISCORD_PREFIX}stats\`` +' '+ '\n' +
-        `\`• ${process.env.DISCORD_PREFIX}server\`` +' '+ '\n' + 
-        `\`• ${process.env.DISCORD_PREFIX}user @user\`` +' ' + '\n' +
-        `\`• ${process.env.DISCORD_PREFIX}ping\``)
-
-        .addField("`❯ MOD - KICK / BAN / MUTE / UNMUTE A USER FROM THE SERVER`", `\`• ${process.env.DISCORD_PREFIX}kick @user reason\`` +' '+ '\n' +
-        `\`• ${process.env.DISCORD_PREFIX}ban @user reason\`` +' ' + '\n' +
-        `\`• ${process.env.DISCORD_PREFIX}mute @user reason\`` +' '+ '\n' +
-        `\`• ${process.env.DISCORD_PREFIX}unmute @user\``)
-
-        .addField('`❯ ADMIN - ADD PERMISSIONS TO THE USER`', `\`• ${process.env.DISCORD_PREFIX}perms @user [add/remove] [verified/staff/coadmin/admin]\``)
-    
-        .addField("`❯ UTILS - CLEAR MESSAGES UPTO 50 AT A TIME, CLASH OF CLANS PLAYER & CLAN LOOKUP`", `\`• ${process.env.DISCORD_PREFIX}clear @user no. of msg\`` +' ' + '\n' +
-        `\`• ${process.env.DISCORD_PREFIX}player #tag\`` +' '+ '\n' +
-        `\`• ${process.env.DISCORD_PREFIX}clan #tag\``)
-
-        .addField("`❯ MEE6 - CHECK YOUR MEE6 RANK AND LEVELS OR POSITION`", `\`\n• !rank\`` +' ' + '\n' +
-        `\`• !levels\``)
-
-        message.channel.send({embed});
-    }
-    
-}
-/*
         .addField("❯ HELP", `\`• ${process.env.DISCORD_PREFIX}help\`` +' '+ `\`It explains itself!\``)
     
         .addField("❯ INFO", `\`• ${process.env.DISCORD_PREFIX}stats\`` +' '+ `\`It shows the bot information\`` + '\n' +
@@ -58,16 +32,23 @@ exports.run = (client, message, args) => {
 
         .addField("❯ MOD", `\`• ${process.env.DISCORD_PREFIX}kick @user\`` +' '+ `\`To kick a user\`` + '\n' +
         `\`• ${process.env.DISCORD_PREFIX}ban @user\`` +' '+ `\`To ban a user\`` + '\n' +
-        `\`• ${process.env.DISCORD_PREFIX}mute @user\`` +' '+ `\`To mute a user for 1 min\`` + '\n' +
+        `\`• ${process.env.DISCORD_PREFIX}mute @user\`` +' '+ `\`To mute a user\`` + '\n' +
         `\`• ${process.env.DISCORD_PREFIX}unmute @user\`` +' '+ `\`To unmute a user\``)
 
         .addField('ADMIN', `\`• ${process.env.DISCORD_PREFIX}perms @user [add/remove] [verified/staff/coadmin/admin]\`` + '\n' +
         `\`• To add special permissions to the user\``)
     
         .addField("❯ UTILS", `\`• ${process.env.DISCORD_PREFIX}clear [optional @user]\`` +' '+ `\`To clear messages\`` + '\n' +
-        `\`• ${process.env.DISCORD_PREFIX}player tag\`` +' '+ `\`It shows the clash player info\`` + '\n' +
-        `\`• ${process.env.DISCORD_PREFIX}clan tag\`` +' '+ `\`It shows clan info\``)
+        `\`• ${process.env.DISCORD_PREFIX}player tag\`` +' '+ `\`Player Lookup\`` + '\n' +
+        `\`• ${process.env.DISCORD_PREFIX}clan tag\`` +' '+ `\`Clan Lookup\``)
 
         .addField("❯ MEE6", `\`\n• !rank\`` +' '+ `\`It shows your rank\`` + '\n' +
         `\`• !levels\`` +' '+ `\`It shows the position\``)
+
+        message.channel.send({embed});
+    }
+    
+}
+/*
+
 */
