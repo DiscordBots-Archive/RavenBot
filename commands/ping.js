be1e5ead11ae1ee23218cc374860802d04715c9e
unknown <suvajit.c@outlook.in>
2018-12-27 20:31:56 +0530

    update

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,8 @@
 exports.run = async (client, message, args) => {
 
-    if (message.channel.name !== 'bot-commands' & message.channel.type !== 'dm') {
-        message.delete(4000);
-        let channel = message.guild.channels.find(ch => ch.name === "bot-commands");
-        if(!channel) return message.channel.send('Could not found **#bot-commands** channel.')
-        return message.channel.send(`Please use commands in appropriate chatrooms to reduce spam ${channel}`).then(msg => {msg.delete(4000)});
-    }
+    message.delete(8000)
+
     const m = await message.channel.send("PING TEST");
-    m.edit(`\`Latency ${m.createdTimestamp - message.createdTimestamp}ms\` <a:hype:515571561345056783> \`API Latency ${Math.round(client.ping)}ms\``);
+    m.edit(`\`Latency ${m.createdTimestamp - message.createdTimestamp}ms\` <a:hype:515571561345056783> \`API Latency ${Math.round(client.ping)}ms\``).then(msg => {msg.delete(8000)});
 
 }
