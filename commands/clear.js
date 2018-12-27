25bd92a8cefb6c967c41458c7e6b231d1c80289f
unknown <suvajit.c@outlook.in>
2018-12-27 19:07:23 +0530

    update

--- commands/clear.js
+++ commands/clear.js
@@ -1,12 +1,21 @@
 exports.run = async (client, message, args) => {
 
     if (message.channel.type == 'dm') return;
-
-    if (message.channel.id === '527084773164843018' && message.channel.id === '527125137959682049' && message.channel.id === '513999069111255040' && message.channel.id === '501395897322831875' && message.channel.id === '521735333658755093' && message.channel.id === '513743066025426945' && message.channel.id === '521739952728440832' && message.channel.id === '517935069172727818'  && message.channel.id === '524139389635395594'  && message.channel.id === '524139508757823498'  && message.channel.id === '524139577011863562') return message.channel.send('This Command id disabled for this channel!')
+    if (message.channel.id === '501395897322831875') return message.channel.send('This Command id disabled for this channel!');
+    if (message.channel.id === '513999069111255040') return message.channel.send('This Command id disabled for this channel!');
+    if (message.channel.id === '527084773164843018') return message.channel.send('This Command id disabled for this channel!');
+    if (message.channel.id === '527125137959682049') return message.channel.send('This Command id disabled for this channel!');
+    if (message.channel.id === '517935069172727818') return message.channel.send('This Command id disabled for this channel!');
+    if (message.channel.id === '524139389635395594') return message.channel.send('This Command id disabled for this channel!');
+    if (message.channel.id === '524139508757823498') return message.channel.send('This Command id disabled for this channel!');
+    if (message.channel.id === '524139577011863562') return message.channel.send('This Command id disabled for this channel!');
+    if (message.channel.id === '521735333658755093') return message.channel.send('This Command id disabled for this channel!');
+    if (message.channel.id === '513743066025426945') return message.channel.send('This Command id disabled for this channel!');
+    if (message.channel.id === '521739952728440832') return message.channel.send('This Command id disabled for this channel!');
 
     if (!message.member.roles.get('500700090181222400') && !message.member.roles.get('500683949018710036')  && !message.member.roles.get('500683658009640975')) {
         message.delete(4000);
-        return message.channel.send(`Only <@&500683949018710036> / <@&500683658009640975> 😒`).then(msg => {msg.delete(5000)});
+        return message.channel.send(`Only <@&500683949018710036> / <@&500683658009640975> use this command`).then(msg => {msg.delete(5000)});
     }
 
     const user = message.mentions.users.first();
@@ -14,11 +23,11 @@ exports.run = async (client, message, args) => {
     const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
     if (!amount) {
         message.delete(4000);
-        return message.channel.send('Please specify a user and amount, or just an amount, of messages to clear.').then(msg => {msg.delete(5000)});
+        return message.channel.send('Please specify a user and amount, or just an amount, of messages to clear').then(msg => {msg.delete(5000)});
     }
     if (!amount && !user) {
         message.delete(4000);
-        return message.reply('Please specify a user and amount, or just an amount, of messages to purge.').then(msg => {msg.delete(5000)});
+        return message.reply('Please specify a user and amount, or just an amount, of messages to purge').then(msg => {msg.delete(5000)});
     }
     message.channel.fetchMessages({
      limit: 50,
