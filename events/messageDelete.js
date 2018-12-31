d7110f0978a2948fcf2f6698d9f7b9a942c54658
unknown <suvajit.c@outlook.in>
2018-12-31 11:25:31 +0530

    update

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -11,20 +11,23 @@ module.exports = async (client, message) => {
     const entry = await message.guild.fetchAuditLogs({type: 'MESSAGE_DELETE'}).then(audit => audit.entries.first())
     if (message.author.bot == true) return;
 	let user = ""
+	let userid = ""
 	  if (entry.extra.channel.id === message.channel.id
 		&& (entry.target.id === message.author.id)
 		&& (entry.createdTimestamp > (Date.now() - 5000))
 		&& (entry.extra.count >= 1)) {
-	  user = entry.executor.tag
+		user = entry.executor.tag
+		userid = entry.executor.id
 	} else { 
-	  user = message.author.tag
-    }
+		user = message.author.tag
+		userid = message.author.id
+  }
 
     const embed = new Discord.RichEmbed()
-    .setTitle(entry.executor.tag + ' | ' + entry.executor.id)
+    .setTitle(user + ' | ' + userid)
     .addField('Channel', '<#' + message.channel.id + '>')
-    .addField(message.author.tag + ' | ' + message.author.id, message)
+    .addField('Content', message)
     .setFooter('Message Deleted', entry.executor.displayAvatarURL)
     .setTimestamp()
-	logs.send({embed});
+		logs.send({embed});
 }
