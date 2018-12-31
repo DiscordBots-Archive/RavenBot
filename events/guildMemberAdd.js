aef6c9c6344eae94b6378322ed96d980e254c27e
unknown <suvajit.c@outlook.in>
2018-12-31 20:14:00 +0530

    up

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -14,20 +14,20 @@ module.exports = (client, member) => {
 
     const channel = member.guild.channels.find(ch => ch.name === 'welcome');
     if (!channel) return;
+    const embedchannel = member.guild.channels.find(ch => ch.name === 'member-log');
+    if (!embedchannel) return;
 
     if (member.guild.id === '500004711005683717') {
         client.channels.get(channel.id).send(`Hello ${member}, Welcome to **${member.guild.name}** :tada:`);
         setTimeout(() => {
             client.channels.get(channel.id).send("If you're new to the server you should only see three chats." + "\n" + "To gain access to this server please react to the post in <#501395897322831875> with the checkmark by clicking it!")
         }, 2000);
-        const channel = member.guild.channels.find(ch => ch.name === 'member-log');
-        if (!channel) return;
-        client.channels.get(channel.id).send({embed});
+
+        client.channels.get(embedchannel.id).send({embed});
 
     } else {
-        const channel = member.guild.channels.find(ch => ch.name === 'member-log');
-        if (!channel) return;
-        client.channels.get(channel.id).send({embed});
+        
+        client.channels.get(embedchannel.id).send({embed});
     }
 
 }
