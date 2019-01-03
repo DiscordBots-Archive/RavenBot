43800fa3300dd404d92a5bf7dc93639d131e41b6
unknown <suvajit.c@outlook.in>
2019-01-03 20:17:36 +0530

    up

---
+++ commands/npm.js
@@ -0,0 +1,46 @@
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+const qs = require('querystring');
+const moment = require('moment');
+const duration = require('moment-duration-format');
+
+module.exports = {
+    name: 'npm',
+	type: 'Utils',
+	usage: ' ',
+	description: 'Server Invite Link',
+	//cooldown: 600,
+	
+	async execute(message, args) {
+        let pkg = args.join(' ')
+        const res = await fetch(`https://registry.npmjs.com/${pkg}`);
+		if (res.status === 404) {
+			return message.channel.reply("Yukikaze couldn't find the requested information. Maybe look for something that actually exists the next time!");
+		}
+		const body = await res.json();
+		if (body.time.unpublished) {
+			return message.channel.reply('whoever was the Commander of this package decided to unpublish it, what a fool.');
+		}
+		const version = body.versions[body['dist-tags'].latest];
+		//const maintainers = this._trimArray(body.maintainers.map(user => user.name));
+		const dependencies = version.dependencies ? this._trimArray(Object.keys(version.dependencies)) : null;
+		const embed = new Discord.RichEmbed()
+			.setColor(0xCB0000)
+			.setAuthor('NPM', 'https://i.imgur.com/ErKf5Y0.png', 'https://www.npmjs.com/')
+			.setTitle(body.name)
+			.setURL(`https://www.npmjs.com/package/${pkg}`)
+			.setDescription(body.description || 'No description.')
+			.addField('❯ Version', body['dist-tags'].latest, true)
+			.addField('❯ License', body.license || 'None', true)
+			.addField('❯ Author', body.author ? body.author.name : '???', true)
+			.addField('❯ Creation Date', moment.utc(body.time.created).format('YYYY/MM/DD hh:mm:ss'), true)
+			.addField('❯ Modification Date', moment.utc(body.time.modified).format('YYYY/MM/DD hh:mm:ss'), true)
+			.addField('❯ Main File', version.main || 'index.js', true)
+			.addField('❯ Dependencies', dependencies && dependencies.length ? dependencies.join(', ') : 'None')
+            //.addField('❯ Maintainers', maintainers.join('; '));
+            
+
+		return message.channel.send(embed);
+	}
+
+};
---
+++ commands/src.js
@@ -0,0 +1,46 @@
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+const qs = require('querystring');
+
+module.exports = {
+    name: 'src',
+	type: 'Utils',
+	usage: ' ',
+	description: 'Server Invite Link',
+	//cooldown: 600,
+	
+	async execute(message, args) {
+        query = args.join(' ');
+		let project = 'main';
+		let branch = ['stable', 'master', 'rpc', 'commando'].includes(query.slice(-1)[0]) ? query.pop() : 'stable';
+		if (['rpc', 'commando'].includes(branch)) {
+			project = branch;
+			branch = 'master';
+		}
+		const queryString = qs.stringify({ q: args.join(' ') });
+		const res = await fetch(`https://djsdocs.sorta.moe/${project}/${branch}/embed?${queryString}`);
+		const embed = await res.json();
+		if (!embed) {
+			return message.channel.reply("Purple couldn't find the requested information. Maybe look for something that actually exists the next time!");
+		}
+		if (message.channel.type === 'dm' || !(message.channel).permissionsFor(message.guild.me).has(['ADD_REACTIONS', 'MANAGE_MESSAGES'], false)) {
+			return message.util.send({ embed });
+		}
+		const msg = await message.channel.send({ embed });
+		msg.react('🗑');
+		let react;
+		try {
+			react = await msg.awaitReactions(
+				(reaction, user) => reaction.emoji.name === '🗑' && user.id === message.author.id,
+				{ max: 1, time: 10000, errors: ['time'] }
+			);
+		} catch (error) {
+			msg.clearReactions();
+
+			return message;
+		}
+		react.first().message.delete();
+
+		return message;
+	},
+};
