f121de1cb5ce0931f2e6e4486041bb552499d0c6
unknown <suvajit.c@outlook.in>
2019-02-06 20:22:13 +0530

    using `dotenv(.env)` instead of `auth.json` folder

--- purple.js
+++ purple.js
@@ -1,7 +1,7 @@
 const PurpleClient = require('./purple/client/purple_client');
-const { token } = require('./auth.json');
-//require('dotenv').config();
-//const token = process.env.TOKEN
+require('dotenv').config();
+const token = process.env.TOKEN;
+
 const client = new PurpleClient();
 
 client.on('error', err => client.logger.error(`[CLIENT ERROR] ${err.message}`, err.stack)).on('warn', warn => client.logger.warn(`[CLIENT WARN] ${warn}`));
