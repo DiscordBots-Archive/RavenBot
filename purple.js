25d79de4d456424c9fdc183e0002d3520c72eebf
unknown <suvajit.c@outlook.in>
2019-02-06 20:29:03 +0530

    deleted an npm package `then`

--- purple.js
+++ purple.js
@@ -4,7 +4,8 @@ const token = process.env.TOKEN;
 
 const client = new PurpleClient();
 
-client.on('error', err => client.logger.error(`[CLIENT ERROR] ${err.message}`, err.stack)).on('warn', warn => client.logger.warn(`[CLIENT WARN] ${warn}`));
+client.on('error', err => client.logger.error(`[CLIENT ERROR] ${err.message}`, err.stack));
+client.on('warn', warn => client.logger.warn(`[CLIENT WARN] ${warn}`));
 
 client.start(token);
 client.metrics();
