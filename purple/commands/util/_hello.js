5c12e8a8f35c97d8821d0666eb13cccd1b7aa347
unknown <suvajit.c@outlook.in>
2019-02-11 11:31:20 +0530

    update

--- purple/commands/util/_hello.js
+++ purple/commands/util/_hello.js
@@ -1,11 +1,13 @@
 const { Command } = require('discord-akairo');
 const moment = require('moment');
-const ms = require('ms')
+const ms = require('ms');
+const ytdl = require('ytdl-core')
+
 
 class HelloCommand extends Command {
     constructor() {
         super('hello', {
-           //aliases: ['ms'],
+           aliases: ['play'],
            description: {
                conntent: 'Hello Command',
                usage: '[command]'
@@ -13,29 +15,12 @@ class HelloCommand extends Command {
            category: 'util',
            typing: true,
            ratelimit: 2,
-           args: [
-            {
-                id: 'duration',
-                type: str => {
-                    if (!str) return null;
-                    const duration = ms(str);
-                    if (duration && duration >= 300000 && !isNaN(duration)) return duration;
-                    return null;
-                },
-                prompt: {
-                    start: (message) => `${message.author}, for how long do you want the mute to last?`,
-                    retry: (message) => `${message.author}, please use a proper time format.`
-                }
-            },
-           ]
         });
     }
 
-    async exec(message, {duration}) {
-        console.log(duration)
-        console.log(new Date(Date.now() + duration))
-        message.util.send(`\`${duration}\`\n\`${new Date(Date.now() + duration)}\``)
-        console.log(moment('2019-02-05T15:37:33.786Z').format("DD-MM-YY kk:mm:ss"))
+    async exec(message) {
+        const connection = await this.client.channels.get('544379786605363200').join();
+        connection.play(ytdl('https://www.youtube.com/watch?v=pHR3ttH5t-w'));
     }
 }
 
