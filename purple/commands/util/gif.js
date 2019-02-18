9ff3ffc3b332d1f2ed9335b50e4a30f99ec36507
unknown <suvajit.c@outlook.in>
2019-02-18 15:17:04 +0530

    update

---
+++ purple/commands/util/gif.js
@@ -0,0 +1,45 @@
+const { Command } = require('discord-akairo');
+const { MessageEmbed, MessageAttachment } = require('discord.js');
+const GphApiClient = require('giphy-js-sdk-core');
+const gif = GphApiClient("N0PLpHHzMvsKB0Y4k9w4amOaeWTHuoBX");
+
+class GifCommand extends Command {
+    constructor() {
+        super('gif', {
+            aliases: ['gif'],
+            description: {
+                content: 'null',
+                usage: '<null>',
+                examples: ['null']
+            },
+            category: 'util',
+            channel: 'guild',
+            ratelimit: 2,
+            args: [
+                {
+                    id: 'query',
+                    match: 'rest'
+                }
+            ]
+        });
+    };
+
+    async exec(message, { query }) {
+        let send;
+        if (query) {
+            const gifs = await gif.search('gifs', { "q": query, "limit" : 1 });
+            for (const data of gifs.data) {
+                console.log(data.images.fixed_height.url);
+                send = data.images.fixed_height.url;
+            }
+        } else {
+            const random = await gif.random('gifs', { "limit" : 1 });
+            console.log(random.data.images.fixed_height_downsampled.gif_url)
+            send = random.data.images.fixed_height_downsampled.gif_url;
+        }
+
+        const embed = new MessageAttachment(send);
+        return message.util.send(embed);
+    };
+};
+module.exports = GifCommand;
