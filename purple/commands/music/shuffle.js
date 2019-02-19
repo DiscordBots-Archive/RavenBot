8e0c8631ed163f0261fa18a0a62bbf9fdff87669
unknown <suvajit.c@outlook.in>
2019-02-19 11:03:51 +0530

    added a ( ; ) only

--- purple/commands/music/shuffle.js
+++ purple/commands/music/shuffle.js
@@ -12,16 +12,16 @@ class ShuffleCommand extends Command {
 			channel: 'guild',
 			ratelimit: 2
 		});
-	}
+	};
 
 	async exec(message) {
 		if (!message.member.voice || !message.member.voice.channel) {
 			return message.util.reply(`you have to be in a voice channel first ${this.client.emojis.get('545968755423838209')}`);
-		}
+		};
 		const queue = this.client.music.queues.get(message.guild.id);
 		await queue.shuffle();
 
 		return message.util.send(`**Shuffled Queue** ${this.client.emojis.get('545870932019773480')}`);
-	}
-}
+	};
+};
 module.exports = ShuffleCommand;
