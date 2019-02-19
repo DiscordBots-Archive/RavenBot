68b63d5692cf043d2e8a9fc6f34bc464ac327beb
unknown <suvajit.c@outlook.in>
2019-02-19 11:05:57 +0530

    added a ( ; ) only

--- purple/commands/music/pause.js
+++ purple/commands/music/pause.js
@@ -11,16 +11,16 @@ class PauseCommand extends Command {
 			channel: 'guild',
 			ratelimit: 2
 		});
-	}
+	};
 
 	async exec(message) {
 		if (!message.member.voice || !message.member.voice.channel) {
 			return message.util.reply(`you have to be in a voice channel ${this.client.emojis.get('545968755423838209')}`);
-		}
+		};
 		const queue = this.client.music.queues.get(message.guild.id);
 		await queue.player.pause();
 
 		return message.util.send(`**Paused** ${this.client.emojis.get('545873319426260993')}`);
-	}
-}
+	};
+};
 module.exports = PauseCommand;
