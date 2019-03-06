cc40305aadea1b97c98d5bd351dc61a73d197c9d
SUVAJIT <suvajit.c@outlook.in>
2019-03-07 00:39:27 +0530

    Update guildMemberRemove.js

--- src/listeners/client/guildMemberRemove.js
+++ src/listeners/client/guildMemberRemove.js
@@ -14,7 +14,7 @@ class GuildMemberRemoveListener extends Listener {
 
 		const memberLog = this.client.settings.get(member.guild, 'memberLog', undefined);
 		if (memberLog) {
-			const embed = this.client.util.embed().setAuthor(`${member.user.tag} (${member.user.id})`, member.user.displayAvatarURl())
+			const embed = this.client.util.embed().setAuthor(`${member.user.tag} (${member.user.id})`, member.user.displayAvatarURL())
 			.setFooter(`User Left`).setTimestamp().setColor('RED');
 			return member.guild.channels.get(memberLog).send(embed);
 		}
