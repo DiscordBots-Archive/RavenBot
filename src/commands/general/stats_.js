b2ddb4fe0de024e129f1412b8fb13839fc9bd1b0
unknown <suvajit.c@outlook.in>
2019-03-13 20:15:47 +0530

    removed duplicate command id

--- src/commands/general/stats_.js
+++ src/commands/general/stats_.js
@@ -5,7 +5,7 @@ const Star = require('../../models/stars');
 
 class StatsCommand extends Command {
 	constructor() {
-		super('stats_', {
+		super('_stats_', {
 			//aliases: ['stats'],
 			category: 'general',
 			clientPermissions: ['EMBED_LINKS'],
