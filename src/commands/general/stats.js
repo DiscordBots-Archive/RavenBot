61681d02eae673bdd66c2647311383b182bd215e
unknown <suvajit.c@outlook.in>
2019-03-13 20:07:46 +0530

    removed number of guilds

--- src/commands/general/stats.js
+++ src/commands/general/stats.js
@@ -17,23 +17,20 @@ class StatsCommand extends Command {
 
     async exec(message) {
 
-        const embed = new MessageEmbed().setColor('RED').setTitle(`${this.client.user.username} Statistics`)
+        const embed = new MessageEmbed().setColor('#8387db').setTitle(`${this.client.user.username} Statistics`)
         .setThumbnail(this.client.user.displayAvatarURL())
         .setURL(`https://discordapp.com/oauth2/authorize?client_id=${this.client.user.id}&scope=bot&permissions=2146958847`)
-
-        .addField("❯ Memory Usage", `• Using : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`+
-        `\n• Free : ${Math.round(os.freemem())} MB`)
-        
+        .addField("❯ Memory Usage", [
+            `• Using : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`,
+            `• Free : ${Math.round(os.freemem())} MB`
+        ]) 
         .addField("❯ Uptime", `• ${moment.duration(this.client.uptime).format("M [months], W [weeks], D [days], H [hrs], m [mins], s [secs]")}`)
-    
-        .addField("❯ General Stats", `\n• Servers : ${this.client.guilds.size}` +
-        `\n• Users : ${this.client.users.size}` + 
-        `\n• Channels : ${this.client.channels.size}` )
-        //.addField('❯ SINCE', `• ${moment(this.client.user.createdAt).format("DD-MM-YY kk:mm")}`)
+        .addField("❯ General Stats", [
+            `• Users : ${this.client.users.size}`,
+            `• Channels : ${this.client.channels.size}`
+        ])
         .addField('❯ Version', `• v${version}`)
-
         .addField("❯ Library", `• [discord.js](https://discord.js.org)[-akairo](https://github.com/1Computer1/discord-akairo)`)
-
         .setFooter('© 2018 ' + this.client.users.get(this.client.ownerID).tag, this.client.users.get(this.client.ownerID).displayAvatarURL())
 
         if (message.channel.type === 'dm' || !(message.channel).permissionsFor(message.guild.me).has(['ADD_REACTIONS', 'MANAGE_MESSAGES'], false)) {
