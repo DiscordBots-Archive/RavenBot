2c47044678b6424456dc5a6925a4a66251d37d34
unknown <suvajit.c@outlook.in>
2019-03-13 20:53:06 +0530

    ping command modified

--- src/commands/general/ping.js
+++ src/commands/general/ping.js
@@ -1,13 +1,13 @@
 const { Command } = require('discord-akairo');
 
 const RESPONSES = [
-	'*No...*',
-	'*Not happening...*',
-	'*Maybe later...*',
-	`*Pong! \`$(ping)ms\` :: \`$(heartbeat)ms\`*`,
-	`*Just you know, I'm not doing this for fun! \n\`$(ping)ms\` :: \`$(heartbeat)ms\`*`,
-	`*Don't think this means anything special! \n\`$(ping)ms\` :: \`$(heartbeat)ms\`*`,
-	`*Can we get on with this already? \n\`$(ping)ms\` :: \`$(heartbeat)ms\`*`
+	'No...',
+	'Not Happening...',
+	'Maybe Later...',
+	`Pong! \`$(ping)ms\` :: \`$(heartbeat)ms\``,
+	`Just you know, I'm not doing this for fun! \n\`$(ping)ms\` :: \`$(heartbeat)ms\``,
+	`Don't think this means anything special! \n\`$(ping)ms\` :: \`$(heartbeat)ms\``,
+	`Can we get on with this already? \n\`$(ping)ms\` :: \`$(heartbeat)ms\``
 ];
 
 class PingCommand extends Command {
