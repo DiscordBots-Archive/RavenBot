56eb9c1462d2933b535720c3f7ae1c9c8a86558d
unknown <suvajit.c@outlook.in>
2019-03-13 23:38:25 +0530

    added uptime command

---
+++ src/commands/general/uptime.js
@@ -0,0 +1,24 @@
+const { Command } = require('discord-akairo');
+const moment = require('moment'); require('moment-duration-format');
+
+class UptimeCommand extends Command {
+    constructor() {
+        super('uptime', {
+            aliases: ['uptime'],
+            category: 'general',
+            clientPermissions: ['EMBED_LINKS'],
+            description: {
+                content: 'Displayes uptime of the bot.'
+            }
+        })
+    }
+
+    exec(message) {
+        const duration = moment.duration(this.client.uptime).format("M [months], W [weeks], D [days], H [hrs], m [mins], s [secs]");
+        const embed = this.client.util.embed()
+        .setAuthor(duration).setColor('#8387db')
+        return message.util.send({ embed });
+    }
+}
+
+module.exports = UptimeCommand;
