4836017b02a552c5a1c2cf6a30055a5c0303660a
unknown <suvajit.c@outlook.in>
2019-03-13 17:49:10 +0530

    added webhook in `messageDelete`  event

--- src/listeners/client/messageDelete.js
+++ src/listeners/client/messageDelete.js
@@ -15,16 +15,25 @@ class MessageDeleteListener extends Listener {
 		if (message.author.bot) return;
 		if (!message.content) return;
 		const guildLogs = this.client.settings.get(message.guild, 'guildLog', undefined);
+		const webhook = new WebhookClient(process.env.WebhookID, process.env.WebhookToken);
+
+		const attachment = message.attachments.first();
+		const embed = new MessageEmbed().setColor(0x824aee)
+			.setAuthor(`${message.author.tag} (${message.author.id})`, message.author.displayAvatarURL())
+			.addField('❯ Channel', message.channel).setThumbnail('https://i.imgur.com/EUGvQJJ.png')
+			.addField('❯ Message', `${message.content.substring(0, 1020)}`);
+		if (attachment) embed.addField('❯ Attachment(s)', attachment.url);
+		embed.setTimestamp(new Date());
+		embed.setFooter('Message Deleted');
+
+		if (webhook) {
+			return webhook.send({
+				embeds: [embed],
+				username: 'Logs: MESSAGE DELETED',
+				avatarURL: 'https://i.imgur.com/EUGvQJJ.png'
+			});
+		}
 		if (guildLogs) {
-			const attachment = message.attachments.first();
-			const embed = new MessageEmbed().setColor(0x824aee)
-				.setAuthor(`${message.author.tag} (${message.author.id})`, message.author.displayAvatarURL())
-				.addField('❯ Channel', message.channel).setThumbnail('https://i.imgur.com/EUGvQJJ.png')
-				.addField('❯ Message', `${message.content.substring(0, 1020)}`);
-			if (attachment) embed.addField('❯ Attachment(s)', attachment.url);
-			embed.setTimestamp(new Date());
-			embed.setFooter('Message Deleted');
-			
 			return this.client.channels.get(guildLogs).send({embed});
 		}
 	}
--- src/listeners/client/messageDeleteBulk.js
+++ src/listeners/client/messageDeleteBulk.js
@@ -15,19 +15,29 @@ class MessageDeleteBulkListener extends Listener {
 
 		if (messages.first().author.bot) return;
 		const guildLogs = this.client.settings.get(messages.first().guild, 'guildLog', undefined);
-		if (guildLogs) {
-			const output = messages.reduce((out, msg) => {
-				const attachment = msg.attachments.first();
-				out += `[${moment.utc(msg.createdTimestamp).format('YYYY/MM/DD hh:mm:ss')}] ${msg.author.tag} (${msg.author.id}): ${msg.cleanContent ? msg.cleanContent.replace(/\n/g, '\r\n') : ''}${attachment ? `\r\n${attachment.url}` : ''}\r\n`;
-				return out;
-			}, '');
-			const embed = new MessageEmbed()
-				.setColor(0x824aee)
-				.setAuthor(`${messages.first().author.tag} (${messages.first().author.id})`, messages.first().author.displayAvatarURL())
-				.addField('❯ Logs', 'See attachment file for full logs (possibly above this embed)')
-				.setTimestamp(new Date()).setThumbnail('https://i.imgur.com/EUGvQJJ.png')
-				.setFooter('Bulk Deleted');
+		const webhook = new WebhookClient(process.env.WebhookID, process.env.WebhookToken);
 
+		const output = messages.reduce((out, msg) => {
+			const attachment = msg.attachments.first();
+			out += `[${moment.utc(msg.createdTimestamp).format('YYYY/MM/DD hh:mm:ss')}] ${msg.author.tag} (${msg.author.id}): ${msg.cleanContent ? msg.cleanContent.replace(/\n/g, '\r\n') : ''}${attachment ? `\r\n${attachment.url}` : ''}\r\n`;
+			return out;
+		}, '');
+		const embed = new MessageEmbed()
+		.setColor(0x824aee)
+		.setAuthor(`${messages.first().author.tag} (${messages.first().author.id})`, messages.first().author.displayAvatarURL())
+		.addField('❯ Logs', 'See attachment file for full logs (possibly above this embed)')
+		.setTimestamp(new Date()).setThumbnail('https://i.imgur.com/EUGvQJJ.png')
+		.setFooter('Bulk Deleted');
+
+		if (webhook) {
+			return webhook.send({
+				embeds: [embed],
+				files: [{ attachment: Buffer.from(output, 'utf8'), name: 'logs.txt' }],
+				username: 'Logs: MESSAGE DELETED BULK',
+				avatarURL: 'https://i.imgur.com/EUGvQJJ.png'
+			});
+		}
+		if (guildLogs) {
 			return this.client.channels.get(guildLogs).send({ embed, files: [{ attachment: Buffer.from(output, 'utf8'), name: 'logs.txt' }] });
 		}
 	}
