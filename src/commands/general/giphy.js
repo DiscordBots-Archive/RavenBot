de890388eda48c92a8dbacd3fb576c79585f6236
unknown <suvajit.c@outlook.in>
2019-03-14 12:02:01 +0530

    added gif command

--- src/commands/general/giphy.js
+++ src/commands/general/giphy.js
@@ -1,12 +1,12 @@
 const { Command } = require('discord-akairo');
 const GphApiClient = require('giphy-js-sdk-core');
-const gif = GphApiClient(process.env.GHIPY);
+const gif = GphApiClient(process.env.GIPHY);
 const qs = require('querystring');
 
 class GifCommand extends Command {
     constructor() {
         super('gif-search', {
-            aliases: ['gif', 'ghipy'],
+            aliases: ['gif', 'giphy'],
             category: 'general',
             clientPermissions: ['ATTACH_FILES', 'EMBED_LINKS'],
             args: [
@@ -21,14 +21,14 @@ class GifCommand extends Command {
     async exec(message, { query }) {
         if (query) {
             const queryString = qs.stringify({ q: query });
-            const res = await fetch(`http://api.giphy.com/v1/gifs/search?${queryString}`, { method: 'GET', headers: { api_key: process.env.GHIPY } });
+            const res = await fetch(`http://api.giphy.com/v1/gifs/search?${queryString}`, { method: 'GET', headers: { api_key: process.env.GIPHY } });
             const gif = await res.json();
             for (const data of gif.data) {
                 const embed = this.client.util.embed().setImage(data.images.original.url);
                 return message.channel.send(embed);
             }
         }
-        const res = await fetch(`http://api.giphy.com/v1/gifs/random`, { method: 'GET', headers: { api_key: process.env.GHIPY } });
+        const res = await fetch(`http://api.giphy.com/v1/gifs/random`, { method: 'GET', headers: { api_key: process.env.GIPHY } });
         const random = await res.json();
         const embed = this.client.util.embed().setImage(random.data.images.original.url);
         return message.channel.send(embed);
