9532f297c29d56dd44b37a693a9f56b66112d80c
unknown <suvajit.c@outlook.in>
2019-03-14 11:08:48 +0530

    moved to general category for now

--- src/commands/general/image.js
+++ src/commands/general/image.js
@@ -6,7 +6,7 @@ class ImageCommand extends Command {
     constructor() {
         super('image', {
             aliases: ['image', 'img'],
-            category: 'fun',
+            category: 'general',
             clientPermissions: ['EMBED_LINKS', 'ATTACH_FILES'],
             args: [
                 {
