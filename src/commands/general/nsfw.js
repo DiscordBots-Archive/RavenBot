1a49aa548f8bf08f22e111dc5b30a18fd40c297f
unknown <suvajit.c@outlook.in>
2019-03-16 12:53:07 +0530

    added page number

--- src/commands/general/nsfw.js
+++ src/commands/general/nsfw.js
@@ -10,13 +10,18 @@ class ImgurCommand extends Command {
             args: [
                 {
                     id: 'query',
-                    match: 'content'
+                    content: 'match'
+                },
+                {
+                    id: 'page',
+                    match: 'rest',
+                    default: 100
                 }
             ]
         })
     }
 
-    async exec(message, { query }) {
+    async exec(message, { query, page }) {
 
         if (message.channel.id !== '555369247161843712') {
             const att = new MessageEmbed().setImage('https://images-ext-1.discordapp.net/external/oh9rSfiKmoYaO46eXnDWDHCtMJOq9Ug6mfkL3p9y1vw/%3Fixlib%3Drb-1.2.1%26ixid%3DeyJhcHBfaWQiOjYxMjYwfQ/https/images.unsplash.com/photo-1471871480126-59ab253c49e9?width=799&height=533');
@@ -27,10 +32,10 @@ class ImgurCommand extends Command {
             query = process.env.QUERY
         }
         query = query.replace(/[^a-zA-Z0-9_]/g, '');
-        const page = Math.floor(Math.random() * 100) + 1;
+        const _page = Math.floor(Math.random() * page.replace(/[^0-9]/g, '')) + 1;
         const image = Math.floor(Math.random() * 100) + 1;
         try {
-            const res = await fetch(`https://api.imgur.com/3/gallery/r/${query}/all/${page}`, { method: 'GET', headers: { Authorization: `Client-ID ${process.env.IMGUR}` }});
+            const res = await fetch(`https://api.imgur.com/3/gallery/r/${query}/all/${_page}`, { method: 'GET', headers: { Authorization: `Client-ID ${process.env.IMGUR}` }});
             const data = await res.json();
             return message.channel.send(data.data[image].link);
         } catch {}
