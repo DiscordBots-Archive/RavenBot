989b2174c8f09f375f458af845dfa6db11fc0888
unknown <suvajit.c@outlook.in>
2019-03-16 10:07:10 +0530

    locked dm

--- src/commands/general/nsfw.js
+++ src/commands/general/nsfw.js
@@ -18,6 +18,11 @@ class ImgurCommand extends Command {
 
     async exec(message, { query }) {
 
+        if (message.channel.id !== '555369247161843712') {
+            const att = new MessageEmbed().setImage('https://images-ext-1.discordapp.net/external/oh9rSfiKmoYaO46eXnDWDHCtMJOq9Ug6mfkL3p9y1vw/%3Fixlib%3Drb-1.2.1%26ixid%3DeyJhcHBfaWQiOjYxMjYwfQ/https/images.unsplash.com/photo-1471871480126-59ab253c49e9?width=799&height=533');
+            return message.channel.send(att);
+        }
+
         const page = Math.floor(Math.random() * 19) + 1;
         //const queryString = qs.stringify({ q: query });
         const res = await fetch(`https://api.imgur.com/3/gallery/r/boobs/top/all/200`, { method: 'GET', headers: { Authorization: `Client-ID ${process.env.IMGUR}` }});
