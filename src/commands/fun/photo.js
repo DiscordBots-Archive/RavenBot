34a51a28e9a935e204f5d4c94e8ee2aec48d613c
unknown <suvajit.c@outlook.in>
2019-03-17 18:46:22 +0530

    eslint-disable-line

--- src/commands/fun/photo.js
+++ src/commands/fun/photo.js
@@ -22,15 +22,17 @@ class PhotoCommand extends Command {
 
     async exec(message, { query }) {
 
-        const page = Math.floor(Math.random() * 20) + 1;
-        const queryString = qs.stringify({ query: query.replace(/[^a-zA-Z0-9]+/gi, ' ') });
-        const res = await fetch(`https://api.unsplash.com/search/photos?per_page=1&page=${page}&${queryString}&${process.env.UNSPLASH}`);
-        const data = await res.json();
-        for (const image of data.results) {
-            const embed = this.client.util.embed()
-            embed.setImage(image.urls.raw);
-            return message.util.send(embed);
-        }
+        try {
+            const page = Math.floor(Math.random() * 20) + 1;
+            const queryString = qs.stringify({ query: query.replace(/[^a-zA-Z0-9]+/gi, ' ') });
+            const res = await fetch(`https://api.unsplash.com/search/photos?per_page=1&page=${page}&${queryString}&${process.env.UNSPLASH}`);
+            const data = await res.json();
+            for (const image of data.results) {
+                const embed = this.client.util.embed()
+                embed.setImage(image.urls.raw);
+                return message.util.send(embed);
+            }
+        } catch {} // eslint-disable-line
     }
 }
 
