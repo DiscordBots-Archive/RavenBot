12bb3e0944cbba38c7bcfa23fb1b8e8e697cf970
unknown <suvajit.c@outlook.in>
2019-03-17 18:54:18 +0530

    eslint-disable-line

--- src/commands/fun/dog.js
+++ src/commands/fun/dog.js
@@ -16,14 +16,16 @@ class DogImageCommand extends Command {
     async exec(message) {
 
         const page = Math.floor(Math.random() * 1000) + 1;
-        const res = await fetch(`https://api.pexels.com/v1/search?query=dog&per_page=1&page=${page}`, { method: 'GET', headers: { Authorization: process.env.PEXEL } });
-        const data = await res.json();
-        for (const photo of data.photos) {
-            const title = photo.url.slice(29, - (photo.id.toString().length + 2)).replace(/-/g, ' ').toLowerCase().replace(/\b(\w)/g, char => char.toUpperCase())
-            const embed = this.client.util.embed().setTitle(title)
-            .setImage(photo.src.original).setURL(photo.src.original);
-            return message.util.send(embed);
-        }
+        try {
+            const res = await fetch(`https://api.pexels.com/v1/search?query=dog&per_page=1&page=${page}`, { method: 'GET', headers: { Authorization: process.env.PEXEL } });
+            const data = await res.json();
+            for (const photo of data.photos) {
+                const title = photo.url.slice(29, - (photo.id.toString().length + 2)).replace(/-/g, ' ').toLowerCase().replace(/\b(\w)/g, char => char.toUpperCase())
+                const embed = this.client.util.embed().setTitle(title)
+                .setImage(photo.src.original).setURL(photo.src.original);
+                return message.util.send(embed);
+            }
+        } catch {} // eslint-disable-line
     }
 }
 
