b33cedcd37080496c7789757a8d6746cc00f1e44
unknown <suvajit.c@outlook.in>
2019-03-17 18:41:10 +0530

    removed `message=>` from promt

--- src/commands/owner/blacklist.js
+++ src/commands/owner/blacklist.js
@@ -18,7 +18,7 @@ class GlobalBlacklistCommand extends Command {
 					match: 'content',
 					type: 'user',
 					prompt: {
-						start: message => `who would you like to blacklist/unblacklist?`
+						start: `who would you like to blacklist/unblacklist?`
 					}
 				}
 			]
