f52a43b19062cd5ddc8be727f1209aa3d2f4c6ea
unknown <suvajit.c@outlook.in>
2019-03-17 18:50:53 +0530

    eslint-disable-line

--- src/commands/fun/nsfw.js
+++ src/commands/fun/nsfw.js
@@ -14,7 +14,7 @@ class ImgurCommand extends Command {
                 }
             ],
             description: {
-                content: 'Receives random nsfw.'
+                content: 'Receives random nsfw xD.'
             }
         })
     }
@@ -22,21 +22,19 @@ class ImgurCommand extends Command {
     async exec(message, { query }) {
 
         if (message.channel.id !== '555369247161843712') {
-            const att = new MessageEmbed().setImage('https://images-ext-1.discordapp.net/external/oh9rSfiKmoYaO46eXnDWDHCtMJOq9Ug6mfkL3p9y1vw/%3Fixlib%3Drb-1.2.1%26ixid%3DeyJhcHBfaWQiOjYxMjYwfQ/https/images.unsplash.com/photo-1471871480126-59ab253c49e9?width=799&height=533');
-            return message.channel.send(att);
+            const embed = new MessageEmbed().setImage('https://images.unsplash.com/photo-1471871480126-59ab253c49e9');
+            return message.channel.send(embed);
         }
 
-        if (!query) {
-            query = process.env.QUERY
-        }
+        if (!query) { query = process.env.QUERY; }
         query = query.replace(/[^a-zA-Z0-9_]/g, '');
-        const _page = Math.floor(Math.random() * 100) + 1;
+        const page = Math.floor(Math.random() * 100) + 1;
         const image = Math.floor(Math.random() * 100) + 1;
         try {
-            const res = await fetch(`https://api.imgur.com/3/gallery/r/${query}/all/${_page}`, { method: 'GET', headers: { Authorization: `Client-ID ${process.env.IMGUR}` }});
+            const res = await fetch(`https://api.imgur.com/3/gallery/r/${query}/all/${page}`, { method: 'GET', headers: { Authorization: `Client-ID ${process.env.IMGUR}` }});
             const data = await res.json();
             return message.channel.send(data.data[image].link);
-        } catch {}
+        } catch {} // eslint-disable-line
     }
 }
 
