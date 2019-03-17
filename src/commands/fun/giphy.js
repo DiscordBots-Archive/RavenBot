4654c8afd9704a0ff0454a6d6199b29f9c9c603c
unknown <suvajit.c@outlook.in>
2019-03-17 18:53:28 +0530

    eslint-disable-line

--- src/commands/fun/giphy.js
+++ src/commands/fun/giphy.js
@@ -23,19 +23,22 @@ class GifCommand extends Command {
     }
 
     async exec(message, { query }) {
-        if (query) {
-            const queryString = qs.stringify({ q: query.replace(/[^a-zA-Z0-9]+/gi, ' ') });
-            const res = await fetch(`http://api.giphy.com/v1/gifs/search?${queryString}`, { method: 'GET', headers: { api_key: process.env.GIPHY } });
-            const gif = await res.json();
-            for (const data of gif.data) {
-                const embed = this.client.util.embed().setImage(data.images.original.url);
-                return message.channel.send(embed);
+
+        try {
+            if (query) {
+                const queryString = qs.stringify({ q: query.replace(/[^a-zA-Z0-9]+/gi, ' ') });
+                const res = await fetch(`http://api.giphy.com/v1/gifs/search?${queryString}`, { method: 'GET', headers: { api_key: process.env.GIPHY } });
+                const gif = await res.json();
+                for (const data of gif.data) {
+                    const embed = this.client.util.embed().setImage(data.images.original.url);
+                    return message.channel.send(embed);
+                }
             }
-        }
-        const res = await fetch(`http://api.giphy.com/v1/gifs/random`, { method: 'GET', headers: { api_key: process.env.GIPHY } });
-        const random = await res.json();
-        const embed = this.client.util.embed().setImage(random.data.images.original.url);
-        return message.channel.send(embed);
+            const res = await fetch(`http://api.giphy.com/v1/gifs/random`, { method: 'GET', headers: { api_key: process.env.GIPHY } });
+            const random = await res.json();
+            const embed = this.client.util.embed().setImage(random.data.images.original.url);
+            return message.channel.send(embed);
+        } catch {} // eslint-disable-line
     }
 }
 
