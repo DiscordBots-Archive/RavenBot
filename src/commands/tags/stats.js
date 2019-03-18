c5f75d966ff24251bee0042ae0f8c3d279fb7b6c
unknown <suvajit.c@outlook.in>
2019-03-18 16:19:37 +0530

    re-modified `tag stats` command

--- src/commands/tags/stats.js
+++ src/commands/tags/stats.js
@@ -1,5 +1,11 @@
 const { Command } = require('discord-akairo');
 const Tags = require('../../models/Tags');
+const { db } = require('../../struct/Database');
+const EMOJIS = ({
+    1: '<:first_place:557144859085897740>',
+    2: '<:second_place:557146273812054026>',
+    3: '<:third_place:557146273732493312>'
+})
 
 class TagStatsCommand extends Command {
     constructor() {
@@ -24,23 +30,84 @@ class TagStatsCommand extends Command {
     async exec(message, { member }) {
 
         if (member) {
+            const allTags = await db.query(`
+                SELECT
+                    MAX("uses") AS toptag,
+                    "authorID",
+                    "name"
+                FROM tags
+                WHERE "guildID" = :guildID AND "authorID" = :authorID
+                GROUP BY "authorID", "name" ORDER BY toptag DESC
+                OFFSET :offset
+                LIMIT :limit
+            `, {
+                type: db.Sequelize.QueryTypes.SELECT,
+                replacements: {
+                    authorID: member.user.id,
+                    guildID: message.guild.id,
+                    offset: 0,
+                    limit: 3
+                }
+            })
             const tags = await Tags.findAll({ where: { authorID: member.user.id, guildID: message.guild.id }});
             const totaluses = tags.reduce((count, c) => {
                 return count + c.uses;
             }, 0);
-            
-            const embed = this.client.util.embed().setAuthor(`${member.user.tag}`, member.user.displayAvatarURL())
-            .addField(`Owned Tags`, tags.length).addField(`Owned Tag Uses`, totaluses)
-            .setFooter(message.guild.name, message.guild.iconURL()).setColor(0x8387db)
+    
+            const users = await Promise.all(allTags.map(async row => {
+                const user = await this.client.users.fetch(row.authorID).catch(() => ({ tag: 'Could not fetch user.'}));
+    
+                return { tag: user.tag, toptag: row.toptag, name: row.name }
+            }))
+    
+            const embed = this.client.util.embed().setColor(0x8387db)
+            .setAuthor(`${member.user.tag}`, member.user.displayAvatarURL())
+    
+            if (users.length) {
+                const desc = users.map(({ tag, toptag, name }, index) => `${EMOJIS[1 + index]} ${name} (${toptag} uses)`);
+                embed.addField(`Owned Tags`, tags.length).addField(`Owned Tag Uses`, totaluses)
+                .addField('Top Tags', desc)
+            }
             return message.util.send({ embed });
         }
+
+        const allTags = await db.query(`
+            SELECT
+                MAX("uses") AS toptag,
+                "authorID",
+                "name"
+            FROM tags
+            WHERE "guildID" = :guildID
+            GROUP BY "authorID", "name" ORDER BY toptag DESC
+            OFFSET :offset
+            LIMIT :limit
+        `, {
+            type: db.Sequelize.QueryTypes.SELECT,
+            replacements: {
+                guildID: message.guild.id,
+                offset: 0,
+                limit: 3
+            }
+        });
         const tags = await Tags.findAll({ where: { guildID: message.guild.id }});
         const totaluses = tags.reduce((count, c) => {
-			return count + c.uses;
+            return count + c.uses;
         }, 0);
-        
-        const embed = this.client.util.embed().setAuthor(`${message.guild.name}`, message.guild.iconURL())
-        .addField(`Total Tags`, tags.length).addField(`Total Uses`, totaluses).setColor(0x8387db)
+
+        const users = await Promise.all(allTags.map(async row => {
+            const user = await this.client.users.fetch(row.authorID).catch(() => ({ tag: 'Could not fetch user.'}));
+
+            return { tag: user.tag, toptag: row.toptag, name: row.name }
+        }))
+
+        const embed = this.client.util.embed().setColor(0x8387db)
+        .setAuthor(`${message.guild.name}`, message.guild.iconURL())
+
+        if (users.length) {
+            const desc = users.map(({ tag, toptag, name }, index) => `${EMOJIS[1 + index]} ${name} (${toptag} uses)`);
+            embed.addField(`Total Tags`, tags.length).addField(`Total Uses`, totaluses)
+            .addField('Top Tags', desc)
+        }
         return message.util.send({ embed });
     }
 }
