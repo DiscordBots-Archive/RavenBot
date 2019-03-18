ac78c66c476798834a41aeaddc803a99edcc99d3
unknown <suvajit.c@outlook.in>
2019-03-18 11:35:15 +0530

    trying to find max value

--- src/commands/tags/stats.js
+++ src/commands/tags/stats.js
@@ -1,6 +1,6 @@
 const { Command } = require('discord-akairo');
 const Tags = require('../../models/Tags');
-const { Op } = require('sequelize');
+const { Op, fn, col } = require('sequelize');
 
 class TagStatsCommand extends Command {
     constructor() {
@@ -24,10 +24,8 @@ class TagStatsCommand extends Command {
 
     async exec(message, { member }) {
 
-        
-
         if (member) {
-            const top = await Tags.findOne({ where: { uses: { [Op.lt] : 10000 }, guildID: message.guild.id, authorID: member.user.id }});
+            const top = await Tags.findAll({ where: { uses: { [Op.lt] : 10000 }, guildID: message.guild.id, authorID: member.user.id }});
             const tags = await Tags.findAll({ where: { authorID: member.user.id, guildID: message.guild.id }});
             const totaluses = tags.reduce((count, c) => {
                 return count + c.uses;
@@ -35,18 +33,18 @@ class TagStatsCommand extends Command {
             
             const embed = this.client.util.embed().setAuthor(`${member.user.tag}`, member.user.displayAvatarURL())
             .addField(`Owned Tags`, tags.length).addField(`Owned Tag Uses`, totaluses)
-            .addField('Top Tag', `${top.name} ( ${top.uses} uses )`)
+            .addField('Top Tag', `${top[0].name} ( ${top[0].uses} uses )`)
             .setFooter(message.guild.name, message.guild.iconURL()).setColor(0x8387db)
             return message.util.send({ embed });
         }
-        const top = await Tags.findOne({ where: { uses: { [Op.lt] : 10000 }, guildID: message.guild.id }});
+        const top = await Tags.findAll({ where: { uses: { [Op.lt] : 10000 }, guildID: message.guild.id }});
         const tags = await Tags.findAll({ where: { guildID: message.guild.id }});
         const totaluses = tags.reduce((count, c) => {
 			return count + c.uses;
         }, 0);
         
         const embed = this.client.util.embed().setAuthor(`${message.guild.name}`, message.guild.iconURL())
-        .addField(`Total Tags`, tags.length).addField(`Total Uses`, totaluses).addField('Top Tag', `${top.name} ( ${top.uses} uses)`).setColor(0x8387db)
+        .addField(`Total Tags`, tags.length).addField(`Total Uses`, totaluses).addField('Top Tag', `${top[0].name} ( ${top[0].uses} uses)`).setColor(0x8387db)
         return message.util.send({ embed });
     }
 }
