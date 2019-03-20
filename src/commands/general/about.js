cf612fd2610070b55aec9a985ffc7f5650ec342a
unknown <suvajit.c@outlook.in>
2019-03-20 18:22:28 +0530

    added a new .........

--- src/commands/general/about.js
+++ src/commands/general/about.js
@@ -1,6 +1,7 @@
 const { Command } = require('discord-akairo');
 const Akairo = require('discord-akairo');
 const Discord = require('discord.js');
+const fs = require('fs');
 
 class AboutCommand extends Command {
 	constructor() {
@@ -15,6 +16,7 @@ class AboutCommand extends Command {
 	exec(message) {
 		const prefix = this.handler.prefix(message);
 		const owner = this.client.users.get(this.client.ownerID);
+		const raven = fs.readFileSync('file.txt', 'utf8');
 
 		const embed = this.client.util.embed()
 			.setColor('#8387db')
@@ -22,7 +24,10 @@ class AboutCommand extends Command {
 			.setDescription([
 				`${this.client.user.username} is developed by [${owner.tag}](https://github.com/isuvajit)`,
 				`${this.client.user.username} uses the **[Discord.js](https://discord.js.org) (${Discord.version})** library and the **[Akairo](https://1computer1.github.io/discord-akairo) (${Akairo.version})** framework.`,
-				`Use \`${prefix}stats\` for statistics and \`${prefix}invite\` for an invite link.`
+				`Use \`${prefix}stats\` for statistics and \`${prefix}invite\` for an invite link.`,
+				'```js',
+				`${raven.toString()}`,
+				'```'
 			]);
 
 		return message.util.send({ embed });
