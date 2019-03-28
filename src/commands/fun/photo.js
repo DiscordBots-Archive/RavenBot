4b994e6b1878ba02970c8adfb826e4d27292bfdd
unknown <suvajit.c@outlook.in>
2019-03-28 13:02:04 +0530

    Initial commit

--- src/commands/fun/photo.js
+++ src/commands/fun/photo.js
@@ -23,7 +23,6 @@ class PhotoCommand extends Command {
     }
 
     async exec(message, { query }) {
-
         try {
             const page = Math.floor(Math.random() * 20) + 1;
             const queryString = qs.stringify({ query: query.replace(/[^a-zA-Z0-9]+/gi, ' ') });
