a120dd3f66b2325267e8ddc3e8774191b0208085
Suvajit <suvajit.c@outlook.com>
2019-03-31 21:52:02 +0530

    removed useless words

--- src/commands/mod/cybernuke.js
+++ src/commands/mod/cybernuke.js
@@ -102,7 +102,7 @@ class LaunchCybernukeCommand extends Command {
 		await response.reply([
 			'',
 			`__**Fatalities:**__`,
-			`${fatalities.length > 0 ? `${fatalities.length} confirmed KIA. \n${fatalities.map(fat => `**-** ${fat.displayName} (${fat.id})`).join('\n')}` : 'None'}`,
+			`${fatalities.length > 0 ? `${fatalities.length} confirmed. \n${fatalities.map(fat => `**-** ${fat.displayName} (${fat.id})`).join('\n')}` : 'None'}`,
 			'',
 			`${survivors.length > 0 ? `__**Survivors**__ \n${survivors.length} left standing. \n${survivors.map(srv => `**-** ${srv.member.displayName} (${srv.member.id}): \`${srv.error}\``).join('\n')}` : ''}`
 		], { split: true });
