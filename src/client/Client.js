57ab395a479cedd9cba3eeef806937d384419e62
Suvajit <suvajit.c@outlook.com>
2019-04-12 11:13:41 +0530

    added more docs

--- src/client/Client.js
+++ src/client/Client.js
@@ -16,6 +16,9 @@ const { Op } = require('sequelize');
 const { parse } = require('url');
 const path = require('path');
 
+/**
+ * Client
+ */
 class Client extends AkairoClient {
 	constructor(config) {
 		super({ ownerID: config.owner }, {
