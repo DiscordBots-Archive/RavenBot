bf7f4a25b4ff6d767ce98244a2b090656f2031a7
Suvajit <suvajit.c@outlook.com>
2019-04-26 21:20:29 +0530

    fix: breaking changes of beta.8

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # RavenBot
-(Deprecated Right Now. But I'm making it again, according to new version of Akairo) A discord bot with fully functional music system, in-depth moderation commands, tag system, custom commands and  much more. Made in JavaScript and discord.js library &amp; discord-akairo framework.
-
-## THIS REPO IS INCOMPLETE RIGHT NOW
+A discord bot with fully functional music system, in-depth moderation commands, tag system, custom commands and  much more. Made in JavaScript and discord.js library &amp; discord-akairo framework.
--- src/commands/general/about.js
+++ src/commands/general/about.js
@@ -1,7 +1,6 @@
 const { Command } = require('discord-akairo');
 const Akairo = require('discord-akairo');
 const Discord = require('discord.js');
-const fs = require('fs');
 
 class AboutCommand extends Command {
 	constructor() {
@@ -14,23 +13,16 @@ class AboutCommand extends Command {
 	}
 
 	exec(message) {
-		const prefix = this.handler.prefix(message);
 		const owner = this.client.users.get(this.client.ownerID);
-		const raven = fs.readFileSync('file.txt', 'utf8');
 
 		const embed = this.client.util.embed()
 			.setColor(0x8387db)
 			.setTitle(`About ${this.client.user.username}`)
 			.addField('Dev', `**${this.client.user.username}** is developed by **[${owner.tag}](https://almostsuvajit.xyz)**`)
-			.addField('Core', `[Discord.js](https://discord.js.org) (${Discord.version}) library and [Akairo](https://1computer1.github.io/discord-akairo) (${Akairo.version.replace(/beta.1/g, 'dev')}) framework`)
+			.addField('Core', `[Discord.js](https://discord.js.org) (${Discord.version}) library and [Akairo](https://1computer1.github.io/discord-akairo) (${Akairo.version}) framework`)
 			.addField('Music', '[Lavalink](https://github.com/lavalibs/lavalink.js) audio player, along with [Lavaqueue](https://github.com/lavalibs/lavaqueue), backed by [Redis](https://github.com/MicrosoftArchive/redis) instance')
 			.addField('Database', '[PostgreSQL](https://www.postgresql.org/) database, along with [Sequelize](http://docs.sequelizejs.com/) [Node.js](https://nodejs.org/en/) ORM')
-			.addField('Host', [
-				'[Amazon EC2](https://aws.amazon.com/ec2/)',
-				'```js',
-				`${raven.toString()}`,
-				'```'
-			]);
+			.addField('Host', '[Amazon EC2](https://aws.amazon.com/ec2/)');
 		return message.util.send({ embed });
 	}
 }
